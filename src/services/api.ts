c3b712b014f9eaf1c61518422cb04e5684734a3e
Nailson97 <nailsonsousa97@gmail.com>
2023-02-23 12:18:59 -0300

    feat: refreshing the token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,55 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies()
+interface AxiosErrorResponse {
+  code?: string;
+}
+
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
   headers: {
-    Authorization: `Bearer ${cookies['auth.token']}`
-  }
+    Authorization: `Bearer ${cookies["auth.token"]}`,
+  },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError<AxiosErrorResponse>) => {
+    if (error.response.status == 401) {
+      if (error.response.data?.code == "token.expired") {
+        cookies = parseCookies();
+
+        const { "auth.refreshToken": refreshToken } = cookies;
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data;
+
+            setCookie(undefined, "auth.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+
+            setCookie(
+              undefined,
+              "auth.refreshToken",
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              }
+            );
+
+            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+          });
+      } else {
+      }
+    }
+  }
+);
