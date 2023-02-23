b74476836da03acd47eb77122fb265a172f53b01
Nailson97 <nailsonsousa97@gmail.com>
2023-02-23 16:26:15 -0300

    feat: queue of requests in Axios

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,7 @@
 import { api } from "@/services/api";
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { setCookie, parseCookies } from 'nookies'
+import { setCookie, parseCookies } from "nookies";
 import Router from "next/router";
-
 
 type SigninCredentials = {
   email: string;
@@ -14,63 +13,62 @@ interface AuthProviderProps {
 }
 
 interface User {
-  email: string,
-  permissions: string[]
-  roles: string[]
+  email: string;
+  permissions: string[];
+  roles: string[];
 }
 
 interface AuthContext {
   signIn(credentials: SigninCredentials): Promise<void>;
   isAuthenticated: boolean;
-  user: User
+  user: User;
 }
 
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User>()
+  const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
   useEffect(() => {
-    const { 'auth.token': token} = parseCookies()
+    const { "auth.token": token } = parseCookies();
 
-    if(token) {
-      api.get('/me').then(response => {
-        const { email, permissions, roles } = response.data
-        setUser({ email, permissions, roles })
-      })
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      });
     }
-  }, [])
+  }, []);
 
   async function signIn({ email, password }: SigninCredentials) {
-
     try {
       const response = await api.post("sessions", {
         email,
         password,
       });
 
-      const {token, refreshToken, permissions, roles} = response.data
-      
-      setCookie(undefined, 'auth.token', token, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
-      
-      setCookie(undefined, 'auth.refreshToken', refreshToken, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: '/'
-      })
+      const { token, refreshToken, permissions, roles } = response.data;
 
-      setUser({ 
+      setCookie(undefined, "auth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setCookie(undefined, "auth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setUser({
         email,
         permissions,
-        roles
-      })
+        roles,
+      });
 
-      api.defaults.headers['Authorization'] = `Bearer ${token}`
-      
-      Router.push('/dashboard')
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,8 @@ interface AxiosErrorResponse {
 }
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -24,30 +26,60 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { "auth.refreshToken": refreshToken } = cookies;
-        api
-          .post("/refresh", {
-            refreshToken,
-          })
-          .then((response) => {
-            const { token } = response.data;
+        const originalConfig = error.config;
 
-            setCookie(undefined, "auth.token", token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: "/",
-            });
+        if (!isRefreshing) {
+          isRefreshing = true;
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
 
-            setCookie(
-              undefined,
-              "auth.refreshToken",
-              response.data.refreshToken,
-              {
+              setCookie(undefined, "auth.token", token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: "/",
-              }
-            );
+              });
 
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+              setCookie(
+                undefined,
+                "auth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
       }
     }
