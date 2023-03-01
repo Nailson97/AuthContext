fd87bd062ccc9c9919d8e58f4de336bfae585025
Nailson97 <nailsonsousa97@gmail.com>
2023-03-01 09:19:43 -0300

    feat: retrieving token on server-side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,56 @@
-import { FormEvent, useContext, useState } from 'react'
-import { AuthContext } from '../context/AuthContext'
-import styles from '../styles/Home.module.css'
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { parseCookies } from "nookies";
+import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
- const [email, setEmail] = useState('')
- const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
- const {signIn} = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
- async function handleSubmit(event: FormEvent) {
-   event.preventDefault()
-   
-  const values = {
-    email,
-    password
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const values = {
+      email,
+      password,
+    };
+
+    signIn(values);
   }
 
-  signIn(values)
- }
-
-  return  (
+  return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value) } />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value) } />
-      <button type='submit'>Entrar</button>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
     </form>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["auth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
